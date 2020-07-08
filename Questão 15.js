459eaeeb2ce3fed9ea01f0b14fd8389e5ca91a2f
Daniel André Lima <daniel-oliveira42@users.noreply.github.com>
2020-07-08 15:20:51 -0300

    Create Questão 15.js

---
+++ Questão 15.js
@@ -0,0 +1,16 @@
+carro = "Hatch"
+
+switch(carro)
+{
+    case "Hatch": console.log("Compra efetuada com sucesso")
+    break
+    case "Sedan": console.log("Tem certeza que não prefere este modelo?")
+        break
+    case "Motocicleta": console.log("Tem certeza que não prefere este modelo?")
+        break
+    case "Caminhonete": console.log("Tem certeza que não prefere este modelo?")
+        break   
+    default:
+        console.log("Não trabalhamos com este tipo de automóvel aqui")
+        break
+}
