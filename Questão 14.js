6bbc356b87ecefa9a10787a5656e42f30b6ed3a6
Daniel André Lima <daniel-oliveira42@users.noreply.github.com>
2020-07-08 15:15:56 -0300

    Create Questão 14.js

---
+++ Questão 14.js
@@ -0,0 +1,14 @@
+fruta = 5
+
+switch(fruta)
+{
+    case "Maçã": console.log("Não vendemos esta fruta aqui")
+    break
+    case "Kiwi": console.log("Estamos com escassez de kiwis")
+        break
+    case "Melancia": console.log("Aqui está, são 3 reais o quilo")
+        break
+    default:
+        console.log("Erro")
+        break
+}
