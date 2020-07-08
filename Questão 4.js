a8e69951ed4a4f25b0e4b421708005c9f9389933
Daniel André Lima <daniel-oliveira42@users.noreply.github.com>
2020-07-08 12:09:57 -0300

    Create Questão 4.js

---
+++ Questão 4.js
@@ -0,0 +1,9 @@
+function divisao(dividendo, divisor)
+{
+    console.log(`Resultado : ${dividendo/divisor}`)
+    console.log(`Resto: ${dividendo%divisor}`)
+}
+
+divisao(10, 2)
+divisao(11, 2)
+divisao(111, 2)
