ea2a917e2547d0a526d43155facc99ac5973e091
Daniel André Lima <daniel-oliveira42@users.noreply.github.com>
2020-07-08 15:10:03 -0300

    Create Questão 13.js

---
+++ Questão 13.js
@@ -0,0 +1,19 @@
+dia = 4
+
+switch(dia)
+{
+    case(1): console.log("Segunda, Dia Útil, Dia de Semana") 
+    break
+    case(2): console.log("Terça, Dia Útil, Dia de Semana") 
+    break
+    case(3): console.log("Quarta, Dia Útil, Dia de Semana") 
+    break
+    case(4): console.log("Quinta, Dia Útil, Dia de Semana") 
+    break
+    case(5): console.log("Sexta, Dia Útil, Dia de Semana") 
+    break
+    case(6): console.log("Sábado, Não é dia Útil, Fim de Semana") 
+    break
+    case(7): console.log("Domingo, Não é dia Útil, Fim de Semana") 
+    break
+}
