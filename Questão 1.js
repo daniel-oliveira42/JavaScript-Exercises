a4dfb47b8d4d961572c90c6e801eed0a8e1edbf1
Daniel André Lima <daniel-oliveira42@users.noreply.github.com>
2020-07-08 11:15:24 -0300

    Questão 1

---
+++ Questão 1.js
@@ -0,0 +1,11 @@
+
+
+function soma(a, b)
+{
+    console.log("Soma: ", a + b)
+    console.log("Subtração: ", a - b)
+    console.log("Multiplicação: ", a * b)
+    console.log("Divisão: ", a / b)
+}
+
+soma(1,2)
