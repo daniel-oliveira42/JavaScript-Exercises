1fce90477d4dfd997b486e9b3acd89b92d51d743
Daniel André Lima <daniel-oliveira42@users.noreply.github.com>
2020-07-08 15:35:28 -0300

    Create Questão 18.js

---
+++ Questão 18.js
@@ -0,0 +1,30 @@
+function numerosPorExtenso(num)
+{
+    switch(num)
+    {
+        case 1:
+            return "Um"
+        case 2:
+            return "Dois"
+        case 3:
+            return "Três"
+        case 4:
+            return "Quatro"
+        case 5:
+            return "Cinco"
+        case 6:
+            return "Seis"
+        case 7:
+            return "Sete"
+        case 8:
+            return "Oito"
+        case 9:
+            return "Nove"
+        case 10:
+            return "Dez"
+        default:
+            return "Insira um número dentro do intervalo (0 a 10)"
+    }
+}
+const num =26
+console.log(numerosPorExtenso(num))
